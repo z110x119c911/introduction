a6a1ff2c2ce1a2faf6a561103d3fa74180fd9073
james HE <james840525@gmail.com>
2020-01-15 18:02:43 +0800

    1/15

---
+++ public/js/all.js
@@ -0,0 +1,25 @@
+"use strict";
+
+$(document).ready(function () {
+  $('.scrollTop').click(function (e) {
+    e.preventDefault();
+    var target = $(this).attr('href');
+    var targetPos = $(target).offset().top;
+    $('html,body').animate({
+      scrollTop: targetPos - 55
+    }, 1000);
+  });
+  $(window).scroll(function () {
+    var scrollPos = $(window).scrollTop();
+    var windowHeight = $(window).height();
+    $('.animate').each(function () {
+      var thisPos = $(this).offset().top;
+
+      if (windowHeight + scrollPos >= thisPos) {
+        $(this).addClass('fade-in');
+      }
+    });
+    $('.bg-image').css('background-position-y', -(scrollPos / 2) + 'px');
+  });
+});
+//# sourceMappingURL=all.js.map
